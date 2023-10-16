92dae0ac636e05946c29ae692855c62f2d7ebda3
Anastasia Ravossel <velnast@gmail.com>
2023-10-16 09:40:37 +0300

    feat: add code

--- task/Program.cs
+++ task/Program.cs
@@ -1,1 +1,45 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите элементы массива, разделенные пробелами: ");
+        string input = Console.ReadLine();
+        string[] inputArray = input.Split(' ');
+
+        string[] resultArray = FilterShortStrings(inputArray);
+
+        Console.WriteLine("Результат:");
+        foreach (string str in resultArray)
+        {
+            Console.WriteLine(str);
+        }
+    }
+
+    static string[] FilterShortStrings(string[] inputArray)
+    {
+        int count = 0;
+        foreach (string str in inputArray)
+        {
+            if (str.Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        string[] resultArray = new string[count];
+        int index = 0;
+
+        foreach (string str in inputArray)
+        {
+            if (str.Length <= 3)
+            {
+                resultArray[index] = str;
+                index++;
+            }
+        }
+
+        return resultArray;
+    }
+}
